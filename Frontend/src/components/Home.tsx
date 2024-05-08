f21ef099ef7d0aa7a8a5f2c458ce36e7b459af57
Yawer091 <yaweratique90@gmail.com>
2024-05-08 20:45:38 +0530

    frontend

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -3,6 +3,11 @@ const Home = () => {
   return (
     <div
       id="HOME"
+      style={{
+        backgroundImage: `url(
+          "https://res.cloudinary.com/enchanting/q_90,f_auto,c_lfill,w_800,h_400,x_w_mul_0.38,y_h_mul_0.41,g_xy_center/enchanting-web/2024/04/Kilchurn-castle-on-Loch-Awe-Scotland-1.jpg"
+        )`,
+      }}
       className="bg-cover bg-center w-full h-[500px] flex justify-center items-center bg-hero-image"
     >
       <TravelForm />
