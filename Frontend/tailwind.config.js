99265dc031fe3ab709838a36d1f8f49b50359c7d
Yawer091 <yaweratique90@gmail.com>
2024-05-08 20:43:01 +0530

    frontend

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,7 +4,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-image": "url('./src/assets/hero.jpg')",
+        "hero-image":
+          "url('https://res.cloudinary.com/enchanting/q_90,f_auto,c_lfill,w_800,h_400,x_w_mul_0.38,y_h_mul_0.41,g_xy_center/enchanting-web/2024/04/Kilchurn-castle-on-Loch-Awe-Scotland-1.jpg')",
       },
     },
   },
