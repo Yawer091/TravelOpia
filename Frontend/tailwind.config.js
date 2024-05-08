df6ede10f7e1a9c55735f057f2ae7f91469c8a02
Yawer091 <yaweratique90@gmail.com>
2024-05-08 20:25:22 +0530

    frontend

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-image": "url('./src/assets/hero.jpg')",
+        "hero-image": "url(`./src/assets/hero.jpg`)",
       },
     },
   },
